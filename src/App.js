5ed0249f8db10b8263893f8db63f6b1dc49eb982
nikdimitriadis <dimitriadisnikos3@gmail.com>
2021-12-21 15:05:52 +0200

    deleted comms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import MainRoutes from "./routes/MainRoutes";
-<<<<<<< HEAD
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -10,16 +9,6 @@ function App() {
       <MainRoutes />
       <Footer />
     </>
-=======
-import './App.scss'
-import './pages/LevelOne.scss'
-
-function App() {
-  return (
-    <div className='App'>
-      <MainRoutes />
-    </div>
->>>>>>> 84c5d62144525e26ad33ab9954966bc179dd4e01
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
-// import useFetch from "../components/hooks/useFetch";
 
 const Header = () => {
   const [searchValue, setSearchValue] = useState("");
-  const [submited, setSubmited] = useState(false);
-
-  //   async function testFetch(name) {
-  //     console.log(name);
-  //     try {
-  //       const response = await fetch(
-  //         `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
-  //       );
-
-  //       if (!response.ok) {
-  //         throw new Error("Something went wrong!");
-  //       }
-
-  //       const data = await response.json();
-  //       console.log(data);
-  //     } catch (err) {
-  //       // setIsLoaded(true);
-  //       // setError(error);
-  //       // console.error(err);
-  //     }
-  //   }
 
   const submitHandler = (e) => {
     e.preventDefault();
     setSearchValue("");
-    // testFetch(searchValue);
-    // setSubmited((prev) => !prev);
   };
 
-  //   console.log(submited);
   const changeHandler = (e) => {
     setSearchValue(() => e.target.value);
   };
@@ -60,27 +34,3 @@ const Header = () => {
 };
 
 export default Header;
-=======
-import { Link } from 'react-router-dom'
-
-const Header = () => {
-    return (
-        <header className="headerFooter">
-            <nav className='Navi'>
-                <p className="fontPlayfair">DRINKS&CHILL</p>
-                <Link to="/">MENU</Link>
-            </nav>
-            <article>
-                <h1 className="fontPlayfair">Cocktails & Getränke</h1>
-                <p>HERZLICH WILLKOMMEN IN DER WELT DER COCKTAILS UND GETRÄNKE</p>
-                <div>
-                    <input type="text" name="" id="" placeholder='type something' />
-                    <input className="button" type="button" value="SEARCH" />
-                </div>
-            </article>
-        </header>
-    );
-}
-
-export default Header;
->>>>>>> 84c5d62144525e26ad33ab9954966bc179dd4e01
