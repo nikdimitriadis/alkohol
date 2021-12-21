d0bbf0979743b271b0d8152e46ecba9a1b206b90
nikdimitriadis <dimitriadisnikos3@gmail.com>
2021-12-21 15:31:35 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import MainRoutes from "./routes/MainRoutes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./App.scss";
+import "./pages/LevelOne.scss";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
       <MainRoutes />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,44 @@
 import { NavLink, Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 const links = [
   {
-    drink: 'Gin',
-    info: 'Get all the Gin'
+    drink: "Gin",
+    info: "Get all the Gin",
   },
   {
-    drink: 'Vodka',
-    info: ' It`s Vodka o`clock'
+    drink: "Vodka",
+    info: " It`s Vodka o`clock",
   },
   {
-    drink: 'Rum',
-    info: 'Get a bottle of Rum,yoho'
+    drink: "Rum",
+    info: "Get a bottle of Rum,yoho",
   },
   {
-    drink: 'Scotch',
-    info: 'So much I like to drink Scotch that sometimes I think my name is Igor Stra-whiskey.'
+    drink: "Scotch",
+    info: "So much I like to drink Scotch that sometimes I think my name is Igor Stra-whiskey.",
   },
   {
-    drink: 'Alkoholfrei',
-    info: 'Heute Abend darf es mal alkoholfrei sein. Einer muss am Ende ja den Taxifahrer spielen.'
+    drink: "Alkoholfrei",
+    info: "Heute Abend darf es mal alkoholfrei sein. Einer muss am Ende ja den Taxifahrer spielen.",
   },
   {
-    drink: 'Zufall',
-    info: 'Lorem ipsum dolor'
-  }
+    drink: "Zufall",
+    info: "Lorem ipsum dolor",
+  },
 ];
 
 const Home = () => {
   return (
     <section>
-      <Header />
-      <section className='overviewItems'>
-
+      <section className="overviewItems">
         {links.map((link, index) => (
-          <NavLink className='overview' key={index} to={`${link.drink}`}>
+          <NavLink className="overview" key={index} to={`${link.drink}`}>
             <h2>{link.drink}</h2>
             <p>{link.info}</p>
           </NavLink>
         ))}
-
       </section>
-      <Footer />
+
       <Outlet />
     </section>
   );
