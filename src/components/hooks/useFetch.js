bb05feccf1e7104d179ac162f71e9dc1b84e1e64
nikdimitriadis <dimitriadisnikos3@gmail.com>
2021-12-22 15:18:05 +0200

    fetch_trouble

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -4,22 +4,24 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(async () => {
-    console.log("trexw");
-    setIsLoading(() => true);
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
+  useEffect(() => {
+    (async () => {
+      console.log("trexw");
+      setIsLoading(() => true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await response.json();
+        setData(() => data);
+        setIsLoading(() => false);
+      } catch (err) {
+        //   setIsLoaded(true);
+        //   setError(error);
+        //   console.error(err);
       }
-      const data = await response.json();
-      setData(() => data);
-      setIsLoading(() => false);
-    } catch (err) {
-      //   setIsLoaded(true);
-      //   setError(error);
-      //   console.error(err);
-    }
+    })();
   }, [url]);
 
   return { data, isLoading };
