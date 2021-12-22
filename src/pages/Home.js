d17be6a4d195996264ef5daf71330908c66ad106
nikdimitriadis <dimitriadisnikos3@gmail.com>
2021-12-22 12:31:46 +0200

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,46 @@
 import { NavLink, Outlet } from "react-router-dom";
 
-const links = ["Gin", "Vodka", "Rum", "Scotch", "Free", "Random"];
+const links = [
+  {
+    drink: "Gin",
+    info: "Get all the Gin",
+  },
+  {
+    drink: "Vodka",
+    info: " It`s Vodka o`clock",
+  },
+  {
+    drink: "Rum",
+    info: "Get a bottle of Rum,yoho",
+  },
+  {
+    drink: "Scotch",
+    info: "So much I like to drink Scotch that sometimes I think my name is Igor Stra-whiskey.",
+  },
+  {
+    drink: "Alkoholfrei",
+    info: "Heute Abend darf es mal alkoholfrei sein. Einer muss am Ende ja den Taxifahrer spielen.",
+  },
+  {
+    drink: "Random",
+    info: "Lorem ipsum dolor",
+  },
+];
 
 const Home = () => {
   return (
-    <div>
-      <div>asdds</div>
-      {links.map((link, index) => (
-        <NavLink key={index} to={`${link}`}>
-          {link}
-        </NavLink>
-      ))}
+    <section>
+      <section className="overviewItems">
+        {links.map((link, index) => (
+          <NavLink className="overview" key={index} to={`${link.drink}`}>
+            <h2>{link.drink}</h2>
+            <p>{link.info}</p>
+          </NavLink>
+        ))}
+      </section>
+
       <Outlet />
-    </div>
+    </section>
   );
 };
 
